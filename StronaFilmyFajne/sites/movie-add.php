895f781858a725d1f5b7128a3f77a055cc46ea4d
Rafal223 <113582561+Rafal223@users.noreply.github.com>
2023-02-21 15:14:55 +0100

    Add files via upload

--- StronaFilmyFajne/sites/movie-add.php
+++ StronaFilmyFajne/sites/movie-add.php
@@ -16,22 +16,19 @@
         <div class="details">Nazwa: <input name="nazwa"><br>
         Typ: <input name="typ"><br>
         Opis: <input name="opis"><br>
-        foto </div>';
+        foto: <input name="foto" type="file"> </div>';
         echo '<input type="submit">';
         echo '<br><a href="../index.php?page=1">Strona Główna</a>';
         echo '</form>';
 
         if($_POST!=NULL)
         {
-            print_r($_POST);
             if($_POST["nazwa"]!="" && $_POST["typ"]!="" && $_POST["opis"]!="")
             {
-                $liczba = count($cos);
-                print_r($liczba);
-                $sqlquery = "INSERT INTO `film` VALUES ('".$liczba."', '".$_POST['nazwa']."', '".$_POST['opis']."','".$_POST['typ']."');";
+                $sqlquery = "INSERT INTO `film` VALUES ('DEFAULT','".$_POST['nazwa']."', '".$_POST['opis']."','".$_POST['typ']."');";
                 $con->query($sqlquery);
 
-                $sqlquery2 = "INSERT INTO `user_has_film` VALUES ('".$_SESSION["id"]."','".$liczba."');";
+                $sqlquery2 = "INSERT INTO `user_has_film` VALUES ('".$_SESSION["id"]."','DEFAULT');";
                 $con->query($sqlquery2);
                 header('location: ../index.php?page=1');
             }
